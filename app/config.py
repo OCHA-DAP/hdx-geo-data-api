26764145a0b94f402ee57a3530499aaf892c8d1d
Max Malynowsky <github@maxmalynowsky.com>
2025-10-13 18:16:06 +0200

    add logging configuration

--- app/config.py
+++ app/config.py
@@ -1,12 +1,16 @@
+from logging.config import fileConfig
 from os import environ, getenv
 
 from dotenv import load_dotenv
 
 load_dotenv(override=True)
 
-PREFIX = getenv("PREFIX", "/api")
 HDX_URL = getenv("HDX_URL", "https://data.humdata.org")
+LOGGING_CONF_FILE = getenv("LOGGING_CONF_FILE", "logging.conf")
+PREFIX = getenv("PREFIX", "/api")
 VECTOR_COMMANDS = "Vector commands"
+
+fileConfig(LOGGING_CONF_FILE)
 
 environ["OGR_GEOJSON_MAX_OBJ_SIZE"] = "0"
 environ["OGR_ORGANIZE_POLYGONS"] = "ONLY_CCW"
