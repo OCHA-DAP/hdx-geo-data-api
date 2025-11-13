6e1fa8499f4ab4877482f239aad2376522d11870
Catalin <catalinflorentinion@gmail.com>
2025-11-13 17:02:36 +0200

    HDX-11218 read prefix from configuration

--- app/middleware/mixpanel_tracking_middleware.py
+++ app/middleware/mixpanel_tracking_middleware.py
@@ -2,7 +2,7 @@ import logging
 
 from fastapi import BackgroundTasks, Request
 
-from app.config import MIXPANEL
+from app.config import MIXPANEL, PREFIX
 from app.middleware.util.util import track_api_call
 
 logger = logging.getLogger(__name__)
@@ -15,7 +15,7 @@ async def mixpanel_tracking_middleware(request: Request, call_next):
     response = await call_next(request)
 
     if MIXPANEL:
-        if request.url.path.startswith("/api"):
+        if request.url.path.startswith(PREFIX):
             background_tasks.add_task(track_api_call, request, response)
     else:
         logger.warning("MIXPANEL_TOKEN environment variable is not set.")
